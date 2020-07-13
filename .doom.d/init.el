250ce22aabcd371e54605ef063ec36ce15104b5c
Alexander Madyankin <alexander@madyankin.name>
2020-07-13 18:00:01 +0300

    Commited via cron Mon Jul 13 15:00:01 UTC 2020

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -158,7 +158,7 @@
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
        yaml              ; JSON, but readable
