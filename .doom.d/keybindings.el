2089b4f3c61ae5db9029cb7c32cf7e042577852b
Alexander Madyankin <alexander@madyankin.name>
2020-07-17 12:00:01 +0300

    Commited via cron Fri Jul 17 09:00:00 UTC 2020

--- .doom.d/keybindings.el
+++ .doom.d/keybindings.el
@@ -1,5 +1,5 @@
 ;;; keybindings.el ---
 
-;; (define-key map (kbd "S-c") 'kill-ring-save)
-;; (define-key map (kbd "S-v") 'yank)
-;; (define-key map (kbd "S-x") 'kill-region)
+(define-key global-map (kbd "S-c") 'kill-ring-save)
+(define-key global-map (kbd "S-v") 'yank)
+(define-key global-map (kbd "S-x") 'kill-region)
