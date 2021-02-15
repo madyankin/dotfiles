fe7c5ea985f2ec85f26d490b0e2fca62b108f1a0
Alexander Madyankin <alexander@madyankin.name>
2021-02-15 14:00:59 +0300

    Commited via cron Mon Feb 15 11:00:59 UTC 2021

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -10,17 +10,6 @@
       load-prefer-newer t
       company-idle-delay nil)
 
-(setq org-bullets-bullet-list '("·")
-      org-support-shift-select t
-      org-catch-invisible-edits 'smart
-      org-directory "/Users/outpunk/Dropbox/Org/"
-      org-link-file-path-type 'relative
-      org-agenda-files (directory-files-recursively "~/Dropbox/Org/" "\\.org$")
-      org-todo-keywords '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)"))
-
-      my-zettelkasten-directory (concat org-directory "zettelkasten")
-      my-journal-directory (concat org-directory "journal")
-      my-org-templates-directory (concat org-directory "utils/templates/"))
 
 
  (global-auto-revert-mode t)
@@ -42,133 +31,7 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
-;; Org related stuff
 
-(global-set-key (kbd "s-=") 'org-capture)
-
-
-(defun org-babel-execute:typescript (body params)
-  (let ((org-babel-js-cmd "npx ts-node < "))
-    (org-babel-execute:js body params)))
-
-(after! org
-  (map! :map org-mode-map
-        :n "M-j" #'org-metadown
-        :n "M-k" #'org-metaup
-        :ne "C-s-<down>" #'org-narrow-to-subtree
-        :ne "C-s-<up>" #'widen)
-
-  (setq
-   org-image-actual-width 400
-   org-capture-templates
-        '(("t" "TODO in Journal" entry
-           (function org-journal-find-location)
-           "** TODO %i%?" :empty-lines 1)
-
-          ("j" "Journal" entry
-           (function org-journal-find-location)
-           "** %(format-time-string org-journal-time-format)\n%i%?" :empty-lines 1)
-
-          ("w" "Week summary" entry
-           (function buffer-file-name)
-           "** %(format-time-string org-journal-date-format)\n%i%?" :empty-lines 1)
-
-          ("n" "New note" plain
-           (file my/new-note-path)
-           "#+TITLE: %i%? \n#+ROAM_ALIAS: \"\" \n#+ROAM_TAGS: \n\n* References: \n"))))
-
-
-(setq my/new-note-timestamp-format "%Y-%m-%dT%H%M%S")
-
-(defun my/new-note-path ()
-  (concat my-zettelkasten-directory
-          "/"
-          (format-time-string my/new-note-timestamp-format)
-          ".org"))
-
-(use-package! org-roam
-  :commands (org-roam-insert org-roam-find-file org-roam)
-
-  :init
-  (setq org-roam-directory my-zettelkasten-directory
-        org-roam-graph-viewer "/usr/bin/open")
-  (map! :leader
-        :prefix "n"
-        :desc "org-roam" "l" #'org-roam
-        :desc "org-roam-insert" "i" #'org-roam-insert
-        :desc "org-roam-switch-to-buffer" "b" #'org-roam-switch-to-buffer
-        :desc "org-roam-find-file" "f" #'org-roam-find-file
-        :desc "org-roam-graph" "g" #'org-roam-graph
-        :desc "org-roam-insert" "i" #'org-roam-insert
-        :desc "org-roam-capture" "c" #'org-roam-capture)
-
-  :config
-  (setq org-roam-capture-templates
-        '(("d" "default" plain (function org-roam--capture-get-point)
-
-           "%? \n\n* References\n\n"
-           :file-name "%(format-time-string my/new-note-timestamp-format)"
-           :head "#+TITLE: ${title} \n#+ROAM_ALIAS: \"\" \n#+ROAM_TAGS: \n\n"
-           :unnarrowed t)))
-  (org-roam-mode +1))
-
-(use-package! ob-C :after org)
-(use-package! ob-emacs-lisp :after org)
-(use-package! ob-java :after org)
-(use-package! ob-js :after org)
-(use-package! ob-makefile :after org)
-(use-package! ob-org :after org)
-(use-package! ob-python :after org)
-(use-package! ob-ruby :after org)
-(use-package! ob-shell :after org)
-
-(use-package! deft
-  :after org
-  :bind ("C-M-S-s-d" . deft)
-  :custom
-  (deft-new-file-format my/new-note-timestamp-format)
-  (deft-recursive t)
-  (deft-default-extension "org")
-  (deft-directory org-directory))
-
-
-(use-package! org-journal
-  :after org
-  :init
-  (map! :leader
-        :prefix "j"
-        :desc "Today journal file" "o" #'org-journal-open-current-journal-file
-        :desc "New journal entry" "j" #'(lambda () (interactive) (org-capture 1 "j"))
-        :desc "New journal todo" "t" #'(lambda () (interactive) (org-capture 1 "t")))
-  :custom
-  (org-journal-file-type 'monthly)
-  (org-journal-file-format "%Y-%m.org")
-  (org-journal-date-format "%A, %d %B %Y")
-  (org-journal-dir my-journal-directory))
-
-;; Org journal fn for capture
-(defun org-journal-find-location ()
-  (org-journal-new-entry t) (goto-char (point-min)))
-
-
-;; Drawing diagrams with Graphviz in org-mode
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((dot . t)))
-
-
-(use-package! org-download
-  :after org
-  :config
-  (setq-default org-download-image-dir "./attachments/")
-  (setq-default org-download-method 'directory)
-  (setq-default org-download-heading-lvl nil)
-  (setq org-download-annotate-function (lambda (_link) ""))
-  (setq-default org-download-timestamp "%Y-%m-%d_%H-%M-%S_"))
-
-
-(use-package ox-hugo
-  :after ox)
 
 
 (setq default-input-method "russian-computer")
@@ -308,6 +171,7 @@ point reaches the beginning or end of the buffer, stop there."
 ;;
 (load! "appearance")
 (load! "keybindings")
+(load! "org")
 (load! "../.config/emacs.private.el")
 
 (custom-set-variables
