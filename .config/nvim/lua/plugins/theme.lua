623762190c1afa665b5c713ee325ce1b97595987
Alexander Madyankin <alexander@madyankin.name>
2024-03-11 18:00:01 +0100

    Commited via cron Mon Mar 11 17:00:01 UTC 2024

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,31 @@
 return {
-  "projekt0n/github-nvim-theme",
+  {
+    "projekt0n/github-nvim-theme",
 
-  lazy = false,
-  priority = 1000,
+    lazy = false,
+    priority = 1000,
 
-  config = function()
-    require("github-theme").setup({})
-    vim.cmd("colorscheme github_dark_dimmed")
-  end,
+    config = function()
+      require("github-theme").setup({})
+      vim.cmd("colorscheme github_dark_dimmed")
+    end,
+  },
+
+  {
+    "f-person/auto-dark-mode.nvim",
+
+    config = {
+      update_interval = 1000,
+
+      set_dark_mode = function()
+        vim.api.nvim_set_option("background", "dark")
+        vim.cmd("colorscheme github_dark_dimmed")
+      end,
+
+      set_light_mode = function()
+        vim.api.nvim_set_option("background", "light")
+        vim.cmd("colorscheme github_light_default")
+      end,
+    },
+  },
 }
