a582b84fc2270e8db79019be92b505613d3783a5
Alexander Madyankin <alexander@madyankin.name>
2024-04-05 23:17:15 +0200

    Commited via cron Fri Apr  5 21:17:15 UTC 2024

--- .config/yadm/scripts/emacs.sh
+++ .config/yadm/scripts/emacs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 brew tap d12frosted/emacs-plus
-brew install emacs-plus --with-nobu417-big-sur-icon
+brew install emacs-plus --with-nobu417-big-sur-icon --with-native-comp
 brew services start d12frosted/emacs-plus/emacs-plus
 
 git clone --depth 1 https://github.com/hlissner/doom-emacs $HOME/.emacs.d
