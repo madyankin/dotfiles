722ed98bf4f4f8fdf29313541d097c34211f4f92
Alexander Madyankin <alexander@madyankin.name>
2024-04-19 16:00:01 +0200

    Commited via cron Fri Apr 19 14:00:01 UTC 2024

--- .config/nvim/lua/plugins/solargraph.lua
+++ .config/nvim/lua/plugins/solargraph.lua
@@ -5,7 +5,21 @@ return {
     opts = {
       ---@type lspconfig.options
       servers = {
-        solargraph = {},
+        solargraph = {
+          cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
+          root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
+          settings = {
+            solargraph = {
+              autoformat = true,
+              completion = true,
+              diagnostic = true,
+              folding = true,
+              references = true,
+              rename = true,
+              symbols = true,
+            },
+          },
+        },
       },
     },
   },
