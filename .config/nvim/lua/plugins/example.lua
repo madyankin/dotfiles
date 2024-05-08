ea64a740d32b91975ba9557e73886ac683c001b5
Alexander Madyankin <alexander@madyankin.name>
2024-05-08 18:00:01 +0200

    Commited via cron Wed May  8 16:00:01 UTC 2024

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -104,7 +104,7 @@ return {
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -146,7 +146,6 @@ return {
         "json",
         "lua",
         "markdown",
-        "markdown_inline",
         "python",
         "query",
         "regex",
