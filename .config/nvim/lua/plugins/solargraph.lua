785681ded33c7de4ddd8816e4b15a41b43561200
Alexander Madyankin <alexander@madyankin.name>
2024-06-27 14:00:00 +0200

    Commited via cron Thu Jun 27 12:00:00 UTC 2024

--- .config/nvim/lua/plugins/solargraph.lua
+++ .config/nvim/lua/plugins/solargraph.lua
@@ -8,14 +8,3 @@ return {
     },
   },
 }
---           cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
---           root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
---           settings = {
---             solargraph = {
---               autoformat = true,
---               completion = true,
---               diagnostic = true,
---               folding = true,
---               references = true,
---               rename = true,
---               symbols = true,
