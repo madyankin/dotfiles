1ec10893a96680d54c3591bf785cb6330b5d462e
Alexander Madyankin <alexander@madyankin.name>
2025-12-19 21:18:13 +0100

    Commited via cron Fri Dec 19 20:18:13 UTC 2025

--- .config/yadm/scripts/linux.sh
+++ .config/yadm/scripts/linux.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-echo "Installing packages"
+set -e
+
+echo "→ Updating packages..."
 sudo apt update
-sudo apt install \
-    build-essentials \
-    gcc \
-    git \
-    bat \
-    neovim \
-    graphviz \
-    texlive-latex-extra \
-    sqlite3
 
+echo "→ Installing packages..."
+sudo apt install -y \
+  build-essential \
+  gcc \
+  git \
+  bat \
+  neovim \
+  sqlite3
 
-echo "Installing fonts..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+echo "→ Installing JetBrains Mono..."
+curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh | bash
--- .config/yadm/scripts/zsh.sh
+++ .config/yadm/scripts/zsh.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-chsh -s /bin/zsh
-git clone https://github.com/zplug/zplug "$HOME/.zplug"
+set -e
 
+# Set zsh as default shell (skip if already set)
+if [[ "$SHELL" != */zsh ]]; then
+  chsh -s /bin/zsh
+fi
+
+# Install zplug (skip if exists)
+if [[ ! -d "$HOME/.zplug" ]]; then
+  git clone --depth 1 https://github.com/zplug/zplug "$HOME/.zplug"
+fi
